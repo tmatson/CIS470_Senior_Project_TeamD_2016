8fc9e7a914b21f439367d0c486b53c73f8303b37
Christopher-Gormley <chris.gormley@icloud.com>
2016-06-12 23:54:21 -0400

    Changed login logic so that at six or more login attempts do not lock out the user.

--- App_Code/clsBusinessLayer.cs
+++ App_Code/clsBusinessLayer.cs
@@ -37,12 +37,23 @@ public class clsBusinessLayer
         currentSession[username] = userAttempts;
 
         //If Statement Whether to Lock Account
-        if ((userAttempts >= 3) || (totalAttempts >= 6))
+//       if ((userAttempts >= 3) || (totalAttempts >= 6))
+//       {
+//            currentSession["LockedSession"] = true;
+//            myDataLayer.LockUserAccount(username);
+//        }
+
+        //
+        //  Chris Gormley
+        //  06/12/2016
+        //  Changed logic so that after total attempts does not lock out user.
+        //
+        if (((userAttempts >= 3) || (totalAttempts >= 6)) && (!isValid))
         {
             currentSession["LockedSession"] = true;
             myDataLayer.LockUserAccount(username);
         }
-        return isValid;
+            return isValid;
     }
 
     //Stores and Finds user information upon login or creating account
